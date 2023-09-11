c7ff6dd59d04abb74cd784e04acaf87e1a79d724
Woochang1132 <show4031132@gmail.com>
2023-09-11 14:08:29 +0900

    최우창 : JavaScript - opacity 조정

--- src/main.js
+++ src/main.js
@@ -22,7 +22,6 @@ const header = document.querySelector('.header');
 const headerHeight = header.getBoundingClientRect().height;
 
 document.addEventListener('scroll', () => {
-    
     //console.log(window.scrollY);
     if(window.scrollY > headerHeight){
         header.classList.add('header--dark');
@@ -31,4 +30,46 @@ document.addEventListener('scroll', () => {
     }
 })
 
+// 스크롤을 진행할 수록 home Section의 요소의 투명도가 달라질 수 있도록
 
+/* 먼저 적용해본 내용
+const home = document.querySelector('.home__container');
+const homeHeight = home.getBoundingClientRect().height;
+
+console.log("home.getBoundingClientRect() >>> " , home.getBoundingClientRect())
+console.log("homeHeight >>>", homeHeight)
+
+
+document.addEventListener('scroll', () => {
+    //console.log(window.scrollY);
+    if(window.scrollY > homeHeight/3){
+        home.classList.remove('home--dark');
+        home.classList.add('home--middleDark');
+    }
+    if(window.scrollY > homeHeight/2){
+        home.classList.remove('home--middleDark');
+        home.classList.add('home--dark')
+    }
+    if(window.scrollY > homeHeight){
+        home.classList.remove('home--middleDark');
+        home.classList.remove('home--dark');
+    }
+})
+*/
+
+// Home 섹션을 아래로 스크롤시 투명하게 처리함
+const home = document.querySelector('.home__container');
+const homeHeight = home.offsetHeight;
+document.addEventListener('scroll', () => {
+/*
+    if(window.scrollY / homeHeight == 0 ){
+        home.style.opacity = 1;
+    }else if(window.scrollY/homeHeight == 0.5){
+        home.style.opacity = 0.5;
+    }else if(window.scrollY/homeHeight == 1){
+        home.style.opacity = 0;
+    }
+*/
+home.style.opacity = 1 - window.scrollY / homeHeight;
+
+});
