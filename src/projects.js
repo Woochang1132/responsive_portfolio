a52ff4a721d0d8b8297676acfd185e040df825ee
Woochang1132 <show4031132@gmail.com>
2023-09-12 16:22:24 +0900

    최우창 : JavaScript - Projects

--- src/projects.js
+++ src/projects.js
@@ -1,1 +1,43 @@
 'use strict';
+
+// 프로젝트 필터링 관련 로직 처리
+
+const categories = document.querySelector('.categories');
+const projects = document.querySelectorAll('.project');
+const projectContainer = document.querySelector('.projects');
+
+categories.addEventListener('click', (event) => {
+    const filter = event.target.dataset.category;
+    if(filter == null){
+        return;
+    }
+    handleAtvieSelection(event.target);
+    filterProjects(filter);
+});
+
+function handleAtvieSelection(target){
+    // Active 메뉴 재설정
+    const active = document.querySelector('.category--selected');
+    active.classList.remove('category--selected');
+    target.target.classList.add('category--selected');
+}
+
+function filterProjects(filter){
+        // 프로젝트 필터링
+    //console.log("filter >>> ", filter)
+   projectContainer.classList.add('anim-out');
+
+   projects.forEach(project => {
+       //console.log("project >>>> ", project.dataset.type);
+
+       if(filter === 'all' || filter === project.dataset.type){
+           project.style.display = 'block';
+       }else{
+           project.style.display = 'none';
+       }
+   });
+   setTimeout(() => {
+       projectContainer.classList.remove('anim-out')
+   }, 250)
+}
+
